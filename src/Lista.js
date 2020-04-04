cea5e21ecb495b3c035c261123c4f56691bca0e6
victor <udemyreactfull@gmail.com>
2020-04-04 06:56:10 -0600

    1.7.firebase-paginar-datosI

--- src/Lista.js
+++ src/Lista.js
@@ -1,13 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from './firebase';
 
 const Lista = (props) => {
+	const [agregar, setAgregar] = useState(false);
+	const [ultimo, setUltimo] = useState(null);
+	const [total, setTotal] = useState(false);
+
 	useEffect(() => {
 		db.collection('posts')
-			.where('categorias', 'array-contains-any', ['angular', 'react'])
+			.where('categorias', 'array-contains-any', ['angular', 'react', 'vue'])
 			.orderBy('creacion', 'desc')
+			.limit(2)
 			.get()
 			.then((querySnapshot) => {
+				let final = querySnapshot.docs[querySnapshot.docs.length - 1];
+				setUltimo(final);
 				querySnapshot.forEach((doc) => {
 					console.log(doc.data());
 				});
@@ -38,6 +45,49 @@ const Lista = (props) => {
 		// 	});
 	}, []);
 
-	return <div></div>;
+	useEffect(() => {
+		if (agregar) {
+			db.collection('posts')
+				.where('categorias', 'array-contains-any', ['angular', 'react'])
+				.orderBy('creacion', 'desc')
+				.startAfter(ultimo)
+				.get()
+				.then((querySnapshot) => {
+					setAgregar(false);
+					let final = querySnapshot.docs[querySnapshot.docs.length - 1];
+					console.log(final);
+					if (final === undefined) {
+						setTotal(true);
+					}
+
+					setUltimo(final);
+					querySnapshot.forEach((doc) => {
+						console.log(doc.data());
+					});
+				})
+				.catch((e) => {
+					setAgregar(false);
+					console.log(e);
+				});
+		}
+	}, [agregar]);
+
+	return (
+		<div>
+			Lista:
+			<br />
+			{total ? (
+				<button disabled>Agregar</button>
+			) : (
+				<button
+					onClick={() => {
+						setAgregar(true);
+					}}
+				>
+					Agregar
+				</button>
+			)}
+		</div>
+	);
 };
 export default Lista;
