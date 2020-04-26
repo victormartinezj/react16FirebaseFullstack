b6e14570e04e63ac2f1372427ea4040832e034a9
victor <udemyreactfull@gmail.com>
2020-04-25 20:20:56 -0500

    routing-app.2.logicaDeNavegacion

--- src/App.js
+++ src/App.js
@@ -1,97 +1,34 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { db, fs } from './firebase';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	Redirect,
+} from 'react-router-dom';
+import FormaRegistro from './formas/FormaRegistro';
+import FormaLogIn from './formas/FormaLogIn';
 import Lista from './Lista';
-import EnviarImg from './EnviarImg';
-import Categorias from './Categorias';
-import FormaPublicacion from './formas/FormaPublicacion';
 
 function App() {
-	const [titulo, setTitulo] = useState('');
-	const [enviando, setEnviando] = useState(false);
-
-	useEffect(() => {
-		if (enviando) {
-			db.collection('posts')
-				.add({
-					titulo,
-					cuerpo: `El cuerpo es: ${titulo}`,
-					categorias: ['angular'],
-					creacion: fs.Timestamp.now(),
-				})
-				.then((docRef) => {
-					setEnviando(false);
-					console.log(`el id es: ${docRef.id}`);
-				})
-				.catch((e) => {
-					setEnviando(false);
-					console.log(`error: ${e}`);
-				});
-			// actualizar
-			// db.collection('posts')
-			// 	.doc('RGWnwhN0RlpHymP7RAXx')
-			// 	.set({ cuerpo: titulo }, { merge: true })
-			// 	.then(() => {
-			// 		setEnviando(false);
-			// 		console.log(`el id es establecido`);
-			// 	})
-			// 	.catch((e) => {
-			// 		setEnviando(false);
-			// 		console.log(`error: ${e}`);
-			// 	});
-
-			// establcer id, cambiar todos datos de un documento
-			// db.collection('posts')
-			// 	.doc('miId')
-			// 	.set({ cuerpo: titulo })
-			// 	.then(() => {
-			// 		setEnviando(false);
-			// 		console.log(`el id es establecido`);
-			// 	})
-			// 	.catch((e) => {
-			// 		setEnviando(false);
-			// 		console.log(`error: ${e}`);
-			// 	});
-			// agregar datos con id autogenerado
-			// db.collection('posts')
-			// 	.add({ titulo })
-			// 	.then((docRef) => {
-			// 		setEnviando(false);
-			// 		console.log(`el id es: ${docRef.id}`);
-			// 	})
-			// 	.catch((e) => {
-			// 		setEnviando(false);
-			// 		console.log(`error: ${e}`);
-			// 	});
-		}
-	}, [enviando]);
-
 	return (
 		<div>
-			<FormaPublicacion />
+			<Router>
+				<Switch>
+					<Route path="/" exact>
+						<Lista />
+					</Route>
+					<Route path="/signup">
+						<FormaRegistro />
+					</Route>
+					<Route path="/login">
+						<FormaLogIn />
+					</Route>
+					<Redirect to="/" />
+				</Switch>
+			</Router>
 		</div>
-		// <div className="App">
-		// 	App
-		// 	<br />
-		// 	<Lista />
-		// 	<input
-		// 		value={titulo}
-		// 		onChange={(e) => {
-		// 			setTitulo(e.target.value);
-		// 		}}
-		// 	/>
-		// 	{enviando ? (
-		// 		<button disabled>Enviando</button>
-		// 	) : (
-		// 		<button
-		// 			onClick={() => {
-		// 				setEnviando(true);
-		// 			}}
-		// 		>
-		// 			Enviar
-		// 		</button>
-		// 	)}
-		// </div>
 	);
 }
 
