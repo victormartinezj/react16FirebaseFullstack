c85c64542a4615b2288c8dcbbb26754288245014
victor <udemyreactfull@gmail.com>
2020-05-16 04:24:27 -0500

    6.7.Formapublicacion

--- src/formas/FormaPublicacion.js
+++ src/formas/FormaPublicacion.js
@@ -6,6 +6,7 @@ import slugify from 'react-slugify';
 import { connect } from 'react-redux';
 import { ACTION_CREAR_NUEVO_POST } from '../state/actions';
 import Categorias from '../Categorias';
+import { Form, Container, Alert } from 'react-bootstrap';
 
 const FormaPublicacion = ({ categoriasServidor, autor, nuevaPublicacion }) => {
 	const {
@@ -51,148 +52,162 @@ const FormaPublicacion = ({ categoriasServidor, autor, nuevaPublicacion }) => {
 
 	return (
 		<div>
-			<form
-				onSubmit={handleSubmit((values) => {
-					nuevaPublicacion({
-						...values,
-						usuario: { nombre: autor.displayName, id: autor.uid },
-					});
-				})}
-			>
-				<label htmlFor="titulo">Título</label>
-				<input
-					name="titulo"
-					ref={register({
-						required: 'El título es requerido',
-						minLength: {
-							value: 10,
-							message: 'El mínimo de longitud es de 10 letras',
-						},
-						maxLength: {
-							value: 20,
-							message: 'El máximo de longitud es de 20 letras',
-						},
+			<Container>
+				<Form
+					className="mt-5"
+					onSubmit={handleSubmit((values) => {
+						nuevaPublicacion({
+							...values,
+							usuario: { nombre: autor.displayName, id: autor.uid },
+						});
 					})}
-					id="titulo"
-				/>
-				<br />
-				{errors.titulo && <p>{errors.titulo.message}</p>}
-				<br />
-				<label htmlFor="resumen">Resumen</label>
-				<br />
-				<textarea
-					id="resumen"
-					name="resumen"
-					ref={register({
-						required: 'El resumen es requerido',
-						minLength: {
-							value: 50,
-							message: 'El mínimo es de 50 caracteres',
-						},
-						maxLength: {
-							value: 200,
-							message: 'El máximo es de 200 caracteres',
-						},
-					})}
-				/>
-				<br />
-				{errors.resumen && <p>{errors.resumen.message}</p>}
-				<br />
-				<label htmlFor="slug">Slug:</label>
-				<br />
-				<input
-					id="slug"
-					name="slug"
-					ref={register({
-						required: 'El slug es requerido',
-						minLength: {
-							value: 20,
-							message: 'El mínimo es de 20',
-						},
-						validate: async (value) => {
-							try {
-								const miSlug = slugify(value);
+				>
+					<Form.Group controlId="titulo">
+						<Form.Label>Título</Form.Label>
+						<Form.Control
+							name="titulo"
+							ref={register({
+								required: 'El título es requerido',
+								minLength: {
+									value: 10,
+									message: 'El mínimo de longitud es de 10 letras',
+								},
+								maxLength: {
+									value: 20,
+									message: 'El máximo de longitud es de 20 letras',
+								},
+							})}
+						/>
+						{errors.titulo && (
+							<Alert className="my-1" variant="danger">
+								{errors.titulo.message}
+							</Alert>
+						)}
+					</Form.Group>
+					<Form.Group controlId="resumen">
+						<Form.Label>Resumen</Form.Label>
+						<Form.Control
+							as="textarea"
+							name="resumen"
+							ref={register({
+								required: 'El resumen es requerido',
+								minLength: {
+									value: 50,
+									message: 'El mínimo es de 50 caracteres',
+								},
+								maxLength: {
+									value: 200,
+									message: 'El máximo es de 200 caracteres',
+								},
+							})}
+						/>
+						{errors.resumen && (
+							<Alert className="my-1" variant="danger">
+								{errors.resumen.message}
+							</Alert>
+						)}
+					</Form.Group>
+					<Form.Group controlId="slug">
+						<Form.Label>Slug:</Form.Label>
 
-								const doc = await db.collection('slugs').doc(miSlug).get();
-								if (doc.exists) {
-									return 'El slug ya existe utiliza otro';
-								} else {
-									return true;
-								}
-							} catch (e) {
-								console.log(e);
-							}
-						},
+						<Form.Control
+							name="slug"
+							ref={register({
+								required: 'El slug es requerido',
+								minLength: {
+									value: 20,
+									message: 'El mínimo es de 20',
+								},
+								validate: async (value) => {
+									try {
+										const miSlug = slugify(value);
+
+										const doc = await db.collection('slugs').doc(miSlug).get();
+										if (doc.exists) {
+											return 'El slug ya existe utiliza otro';
+										} else {
+											return true;
+										}
+									} catch (e) {
+										console.log(e);
+									}
+								},
+							})}
+						/>
+
+						{errors.slug && (
+							<Alert className="my-1" variant="danger">
+								{errors.slug.message}
+							</Alert>
+						)}
+					</Form.Group>
+					<br />
+					<h5>Las categorias seleccionadas son:</h5>
+					{errors.categorias && <p>{errors.categorias.message}</p>}
+					{categorias.map(({ nombre, activa }, index) => {
+						if (activa) {
+							return (
+								<div key={`seleccionada${nombre}`}>
+									<h5>{nombre}</h5>
+									<button
+										onClick={() => {
+											let tempArr = [...categorias];
+											tempArr[index] = { nombre, activa: false };
+											setCategorias(tempArr);
+										}}
+									>
+										cancelar
+									</button>
+								</div>
+							);
+						}
 					})}
-				/>
-				<br />
-				{errors.slug && <p>{errors.slug.message}</p>}
-				<br />
-				<h5>Las categorias seleccionadas son:</h5>
-				{errors.categorias && <p>{errors.categorias.message}</p>}
-				{categorias.map(({ nombre, activa }, index) => {
-					if (activa) {
-						return (
-							<div key={`seleccionada${nombre}`}>
-								<h5>{nombre}</h5>
+					<br />
+					<h5>Categorias:</h5>
+					{categorias.map(({ nombre, activa }, index) => {
+						if (activa) {
+							return (
 								<button
+									disabled
+									type="button"
+									key={nombre}
 									onClick={() => {
 										let tempArr = [...categorias];
-										tempArr[index] = { nombre, activa: false };
+										tempArr[index] = { nombre, activa: true };
 										setCategorias(tempArr);
 									}}
 								>
-									cancelar
+									{nombre}
 								</button>
-							</div>
-						);
-					}
-				})}
-				<br />
-				<h5>Categorias:</h5>
-				{categorias.map(({ nombre, activa }, index) => {
-					if (activa) {
-						return (
-							<button
-								disabled
-								type="button"
-								key={nombre}
-								onClick={() => {
-									let tempArr = [...categorias];
-									tempArr[index] = { nombre, activa: true };
-									setCategorias(tempArr);
-								}}
-							>
-								{nombre}
-							</button>
-						);
-					} else {
-						return (
-							<button
-								type="button"
-								key={nombre}
-								onClick={() => {
-									const tempArr = [...categorias];
-									tempArr[index] = { nombre, activa: true };
-									setCategorias(tempArr);
-								}}
-							>
-								{nombre}
-							</button>
-						);
-					}
-				})}
-				<br />
-				<Categorias />
-				<br />
+							);
+						} else {
+							return (
+								<button
+									type="button"
+									key={nombre}
+									onClick={() => {
+										const tempArr = [...categorias];
+										tempArr[index] = { nombre, activa: true };
+										setCategorias(tempArr);
+									}}
+								>
+									{nombre}
+								</button>
+							);
+						}
+					})}
+					<br />
+					<Categorias />
+					<br />
 
-				<label htmlFor="cuerpo">Cuerpo</label>
-				<Editor setValue={setValue} />
+					<label htmlFor="cuerpo">Cuerpo</label>
+					<Editor setValue={setValue} />
 
-				<br />
-				{errors.cuerpo && <p>{errors.cuerpo.message}</p>}
-				<input type="submit" value="Submit" />
-			</form>
+					<br />
+					{errors.cuerpo && <p>{errors.cuerpo.message}</p>}
+					<input type="submit" value="Submit" />
+				</Form>
+			</Container>
 		</div>
 	);
 };
